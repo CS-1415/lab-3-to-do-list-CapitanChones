ecd1f8d5df2144a5f451b52bcf34277f3414b557
Amando Thomas <angelica.vieyra@gmail.com>
2024-09-18 20:14:45 -0700

    Class Created, Tests Made

---
+++ Tasks.Utilites/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace Tasks.Utilites;
+
+public class Task
+{
+    readonly int id;
+    readonly string title;
+    readonly string description;
+    bool isComplete = false;
+
+    public Task(int _id, string _title, string _description)
+    {
+        id = _id;
+        title = _title;
+        description = _description;
+
+    }
+
+    public static string displayTask(Task tsk)
+    {
+        return" ";
+    }
+
+
+
+
+}
---
+++ Todo.Ui/Program.cs
@@ -0,0 +1,9 @@
+﻿//Amando Thomas 9/18/24 Todo List Lab
+using Tasks.Utilites;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
